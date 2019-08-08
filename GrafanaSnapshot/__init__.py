9adab2c3b792cf61b06723347e761817646f1c71
Authapon Kongkaew <ohmrefresh@gmail.com>
2019-08-08 20:45:14 +0700

    remove un-used

--- GrafanaSnapshot/__init__.py
+++ GrafanaSnapshot/__init__.py
@@ -1,10 +1,2 @@
 # -*- coding: utf-8 -*-
 from .GenerateSnapshot import GenerateSnapshot
-from .version import VERSION
-
-__version__ = VERSION
-__author__ = 'Authapon Kongkaew'
-__email__ = 'ohmrefresh@gmail.com'
-__license__ = 'MIT License'
-__github__ = 'https://github.com/ohmrefresh/GrafanaSnapshotLibrary'
-
